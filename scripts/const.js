4278afdc2288b040ec173a9f4c18a7bd7de47385
Galina Kostina <96977221+Galina1512@users.noreply.github.com>
2023-06-18 23:28:54 +0300

    new API_URL

--- scripts/const.js
+++ scripts/const.js
@@ -1,8 +1,11 @@
 // export const API_URL = 'http://localhost:3000';
-export const API_URL = 'https://knotty-suave-bed.glitch.me';
+// export const API_URL = 'https://knotty-suave-bed.glitch.me';
+export const API_URL = 'https://valuable-yellow-tire.glitch.me/';
 export const JWT_TOKEN_KEY = 'WishList';
 export const ROUTE_NEW_WISH = 'newwish';
 
 // https://first-salt-sorrel.glitch.me/
 
 // https://knotty-suave-bed.glitch.me/
+
+// https://valuable-yellow-tire.glitch.me/
